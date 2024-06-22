118b7f3a454e483843ff423ecc0ebeedb7dc9ffc
OriginalAcademia <marcseuba93@gmail.com>
2024-06-22 13:00:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     canvas.addEventListener('mouseup', stopDrag);
     canvas.addEventListener('mousemove', drag);
 
+    loadCanvasState();
+
     let draggingElement = null;
 
     function startDrag(event) {
@@ -22,8 +24,60 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function stopDrag() {
-        draggingElement = null;
+        if (draggingElement) {
+            saveCanvasState();
+            draggingElement = null;
+        }
     }
+
+    function saveCanvasState() {
+        const elements = Array.from(canvas.children).map(element => {
+            return {
+                type: element.tagName.toLowerCase(),
+                content: element.contentEditable ? element.innerText : element.src || element.srcdoc,
+                style: {
+                    left: element.style.left,
+                    top: element.style.top,
+                    width: element.style.width,
+                    height: element.style.height
+                }
+            };
+        });
+        localStorage.setItem('canvasState', JSON.stringify(elements));
+    }
+
+    function loadCanvasState() {
+        const savedState = localStorage.getItem('canvasState');
+        if (savedState) {
+            const elements = JSON.parse(savedState);
+            elements.forEach(element => {
+                let el;
+                switch (element.type) {
+                    case 'div':
+                        el = document.createElement('div');
+                        el.contentEditable = true;
+                        el.innerText = element.content;
+                        break;
+                    case 'img':
+                        el = document.createElement('img');
+                        el.src = element.content;
+                        break;
+                    case 'iframe':
+                        el = document.createElement('iframe');
+                        el.src = element.content;
+                        break;
+                }
+                el.className = 'draggable';
+                el.style.left = element.style.left;
+                el.style.top = element.style.top;
+                el.style.width = element.style.width;
+                el.style.height = element.style.height;
+                canvas.appendChild(el);
+            });
+        }
+    }
+
+    window.addEventListener('beforeunload', saveCanvasState);
 });
 
 function addText() {
@@ -35,6 +89,7 @@ function addText() {
     textElement.style.left = '50px';
     textElement.style.top = '50px';
     canvas.appendChild(textElement);
+    saveCanvasState();
 }
 
 function addImage() {
@@ -49,6 +104,7 @@ function addImage() {
         imgElement.style.left = '50px';
         imgElement.style.top = '50px';
         canvas.appendChild(imgElement);
+        saveCanvasState();
     }
 }
 
@@ -61,6 +117,9 @@ function addIframe() {
         iframeElement.src = iframeUrl;
         iframeElement.style.left = '50px';
         iframeElement.style.top = '50px';
+        iframeElement.style.width = '300px';
+        iframeElement.style.height = '200px';
         canvas.appendChild(iframeElement);
+        saveCanvasState();
     }
 }
